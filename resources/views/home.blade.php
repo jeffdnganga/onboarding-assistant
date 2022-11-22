dc581327b9b3df5a0e59b90e7c7debce1c217710
Jeff  Nganga <Jeff.Nganga@jubileekenya.com>
2022-11-22 12:58:13 +0300

    ONBO-jeff: Added home page and configured home page route

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Onboarding Assistant</title>
+    <script src="https://kit.fontawesome.com/01b4b6f929.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<h2>Onboarding Assistant</h2>
+
+</body>
+</html>
