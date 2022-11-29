bc386da26d1be3db29608dabe0884f2658c0375c
Cyborg1999 <andrewk.tonui@gmail.com>
2022-11-29 21:48:29 +0300

    update section

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,67 @@
-<section>
+<section id = "about">
     <h1> About Us </h1>
+
+    <div id = "project_description">
+        <p>
+
+        </p>
+
+
+    </div>
+    <div id = "members">
+        <div id = "andrew">
+            <img src = "#">
+            <h3> Andrew Kipkoech</h3>
+
+            <div id ="social_contacts">
+                <ul>
+                    <li>
+                      <a href="https://www.linkedin.com/in/andrew-rotich-5b3254167/"><span class="ico-circle"><i
+                            class="bi bi-linkedin"></i></span></a>
+                    </li>
+                    <li>
+                      <a href="https://github.com/Cyborg1999"><span class="ico-circle"><i
+                            class="bi bi-github"></i></span></a>
+                    </li>
+                    <li>
+                      <a href="https://twitter.com/andrewkipkoech8"><span class="ico-circle"><i
+                            class="bi bi-twitter"></i></span></a>
+                    </li>
+                  </ul>
+
+            </div>
+        </div>
+
+        <div id = "jeff">
+            <img src ="#">
+            <h3> Jeff Ng'ang'a</h3>
+
+            <div id ="social_contacts">
+                <ul>
+                    <li>
+                      <a href="https://www.linkedin.com/in/jeffdnganga/"><span class="ico-circle"><i
+                            class="bi bi-linkedin"></i></span></a>
+                    </li>
+                    <li>
+                      <a href="https://github.com/jeffdnganga"><span class="ico-circle"><i
+                            class="bi bi-github"></i></span></a>
+                    </li>
+                    <li>
+                      <a href="https://twitter.com/jeffy_daniels"><span class="ico-circle"><i
+                            class="bi bi-twitter"></i></span></a>
+                    </li>
+                  </ul>
+
+            </div>
+        </div>
+        <div id = "group project">
+            <h3>group project link</h3>
+            <a href="https://github.com/jeffdnganga/onboarding-assistant">
+
+        </div>
+
+    </div>
+
 
 
 </section>
