0f09d2180631264fbe333f9303b04ba4f1ce15d0
Cyborg1999 <andrewk.tonui@gmail.com>
2022-11-30 12:01:50 +0300

    feature description complete

--- resources/views/features.blade.php
+++ resources/views/features.blade.php
@@ -11,17 +11,31 @@
             <div id = "onboarding_status">
                 <img src ="#">
                 <h3>Onboarding Status</h3>
-                <p></p>
+                <p>
+                    This is the feature that will enable the HR team to keep in
+                    track with your progress. The Onbording status will help
+                    the HR to know if the new employee requires assistance or
+                    has completed the process.
+                </p>
             </div>
             <div id = "progress_bar">
                 <img src ="#">
                 <h3>Progress Bar</h3>
-                <p></p>
+                <p>
+                    This is the feature that will enable the new employees to
+                    keep in track with their progress.
+                </p>
             </div>
             <div id = "resource_checklist">
                 <img src ="#">
                 <h3>Resource Checklist</h3>
-                <p></p>
+                <p>
+                    As new employees request for resources from their employers,
+                    the checklist will be updated appropriately by the new
+                    employees this will enable the new employee and the the HR
+                    team to view progress and respond accordingly.
+
+                </p>
             </div>
 
 
